cb1e443e4952553f0262225bfe89a1b13fb97f49
Won Jongnam <jnwon.stephan@gmail.com>
2026-04-09 18:28:22 +0900

    access token automated setting 유지기능 구현

--- js/content-script.js
+++ js/content-script.js
@@ -1,5 +1,11 @@
 $(document).ready(async () => {
-    const RESPONSE_THRESHOLD = 3;
+    let token = "";
+
+    await chrome.storage.local.get(['token'], (item) => {
+        token = item.token == null || undefined? "" : item.token;
+    });
+
+    const RESPONSE_THRESHOLD = 5;
     const option = {
         attributes: false,
         characterData: false,
@@ -11,6 +17,11 @@ $(document).ready(async () => {
     opBlockObserver.observe(document.getElementById('swagger-ui'), option);
 
     function getTokenAndSet() {
+        if(token != "") {
+            const healthCheck = setToken(token);
+            if(healthCheck) token = "";
+        }
+        
         $('.responses-wrapper span.headerline').each((index, item) => {
             if(item.innerText.indexOf('date: ') >= 0) {
                 const responseOffset = (Date.now() - Date.parse(item.innerText.split('date: ')[1]))/1000;
@@ -18,16 +29,32 @@ $(document).ready(async () => {
                     const $opBlockRoot = $(item).closest('.opblock-post');
                     const apiNameString = $opBlockRoot.find('.opblock-summary-control').first().text();
                     if(apiNameString.indexOf('/login') > 0 && $opBlockRoot.find('.live-responses-table tbody td.response-col_status').first().text() == '200') {
-                        const jsonString = $opBlockRoot.find('.live-responses-table code.language-json').text();
-                        const accessToken = JSON.parse(jsonString).accessToken;
-                        $('button.btn.authorize.unlocked').first().click();
-                        $('#auth-bearer-value').val(accessToken);
-                        $('#auth-bearer-value').focus();
-                        const htmlToAppend = '<section style="color:salmon"><b>스페이스바를 한 번 누른 후 Authorize 버튼을 누르세요!</b></section>';
-                        $('#auth-bearer-value').parent().append(htmlToAppend);
+                        chrome.storage.local.get(['tokenSavelessOption'], (item) => {
+                            tokenSavelessOption = item.tokenSavelessOption == null || undefined? false : item.tokenSavelessOption;
+                            const jsonString = $opBlockRoot.find('.live-responses-table code.language-json').text();
+                            const accessToken = JSON.parse(jsonString).accessToken;
+                            const health = setToken(accessToken);
+                            if(!tokenSavelessOption && health) {
+                                const apiInfo = apiNameString.replace('\/', " \/").replace('\/login', "\/login ");
+                                chrome.storage.local.set({token: accessToken, apiInfo: apiInfo});
+                            }
+                        });
                     }
                 }
             }
         })
     }
+
+    $(document).on('click', 'button.btn.modal-btn.auth.authorize.button', function() {
+        $('#auth-bearer-value').closest('.wrapper').find('label').first().html('Value:')
+    })
 });
+
+function setToken(accessToken) {
+    $('button.btn.authorize.unlocked').first().click();
+    $('#auth-bearer-value').val(accessToken);
+    $('#auth-bearer-value').focus();
+    const htmlToAppend = '<span style="color:salmon"><b>스페이스바를 한 번 누른 후 Authorize 버튼을 누르세요!</b></span>';
+    $('#auth-bearer-value').closest('.wrapper').find('label').first().html(htmlToAppend);
+    return $('#auth-bearer-value').val() != undefined? true : false;
+}
--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,40 @@
 $(document).ready(async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  let apiInfo = "";
+  let tokenSavelessOption = false;
+
+  await chrome.storage.local.get(['apiInfo', 'tokenSavelessOption'], (item) => {
+    apiInfo = item.apiInfo == null || undefined? "" : item.apiInfo;
+    tokenSavelessOption = item.tokenSavelessOption == null || undefined? false : item.tokenSavelessOption
+
+    if(apiInfo != "") {
+      $('#token_dashboard').html('<span style="color:blue">Access 토큰 저장됨</span><br>(' + apiInfo + ')');
+      $('#delete_token').show();
+    }
+    $('#is_token_saveless').prop('checked', tokenSavelessOption);
+  });
+
+  // END OF INIT.
+
+
+  $('#delete_token').on("click", () => {
+    clearStorage();
+  });
+
+  $('#is_token_saveless').on("click", () => {
+    if($('#is_token_saveless').is(':checked')) {
+      clearStorage();
+      tokenSavelessOption = true;
+    }
+    else {
+      tokenSavelessOption = false;
+    }
+    chrome.storage.local.set({tokenSavelessOption: tokenSavelessOption});
+  });
+
 
   $(".iconImg").on("click", () => {
-    $(".iconImg").toggle();
+    // $(".iconImg").toggle();
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: toggleSectionsExpand
@@ -10,6 +42,12 @@ $(document).ready(async () => {
   });
   
 });
+
+function clearStorage() {
+  chrome.storage.local.remove(['token', 'apiInfo']);
+  $('#token_dashboard').html("");
+  $('#delete_token').hide();
+}
 
 function toggleSectionsExpand() {
   document.querySelectorAll("section .opblock-tag .expand-operation").forEach(btn => btn.click());
